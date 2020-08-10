eb018ae2c900a72a7b5043f2dc9f216fb5b7425c
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-10 18:47:40 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,10 @@
 class Cat
+  attr_reader :name 
+  attr_accessor :mood 
   
-  # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervouse"
+    end 
+    # code goes here
 end
