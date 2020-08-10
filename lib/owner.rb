d2f714322154c0ced12e7a9f3a29dc6ff55dfc25
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-10 18:54:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,19 +1,4 @@
 class Owner
-  attr_accessor :name, :prts, :dog, :cat 
-  attr_reader :species 
-  
-  @@all = []
-  @@pets = {:dogs => [], :catsv=> []}
-  
-  def initialize(species)
-    @species = species
-    @name = name
-    @@all << self # code goes here
-  end
-  
-  def self.all
-    @@all
-  end 
   
   
 end
